d5eae3e00b733297e582457808d002a15d32bf5e
shengli1989 <shengli@evendesign.tw>
2013-09-14 18:56:15 +0800

    capacity animation

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,3 +1,4 @@
+// scrollspy
 $(window).load(function() {
   var windowHeight = $(window).height();
   var scrollOffset = windowHeight / 2;
@@ -8,13 +9,33 @@ $(window).load(function() {
 })
 
 
+// carousel
 $('#future-demo').carousel({
   interval: false
 });
 
 
+// fittext
 jQuery(".block-title").fitText(1.5, { minFontSize: '24px', maxFontSize: '50px' });
 jQuery(".block-sub-title").fitText(2, { minFontSize: '24px', maxFontSize: '50px' });
 
 
+// capacity-count
+var capacity_count = $("#capacity-count .count");
+var count;
+
+$('.capacity').on('activate.bs.scrollspy', function () {
+  count = 0;
+  countUp();
+});
+
+function countUp() {
+  capacity_count.text(count.toFixed(2));
+  count = count + 0.125;
+  if (count > 3) {
+    capacity_count.text(3.15);
+    return false;
+  }
+  setTimeout(countUp, 25);
+}
 
