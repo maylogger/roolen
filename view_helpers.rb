c9708e2cc83d669b7f3c8fe9c4267c5361d1f11f
evenwu <evendesign@gmail.com>
2013-09-24 21:13:01 +0800

    刪除不必要設定

--- view_helpers.rb
+++ view_helpers.rb
@@ -1,23 +1,10 @@
 module ViewHelpers
 
-  # capture for slim
-  def capture_slim(&block)
-    old_buffer = eval  "_buf", block.binding
-    eval  "origin_buf,_buf=_buf,[]", block.binding
-    capture_content = yield.to_s
-    eval  "_buf=origin_buf", block.binding
-    capture_content
-  ensure
-    eval  "_buf=origin_buf", block.binding
-  end
-
-
   # yaml data
   def data
     d||=YAML.load_file(File.join(File.dirname(__FILE__), 'data.yml'))
     return d
   end
-
 
   # nav li active link
   def nav_link_to(name, href, options={})
