e3901f67be9fb66de87c7cd5890621df92d4dbdd
shengli1989 <shengli@evendesign.tw>
2013-09-26 22:12:18 +0800

    add 3 way pills

--- javascripts/all.js
+++ javascripts/all.js
@@ -17,7 +17,7 @@ $('.header').affix({
 
 
 // carousel
-$('#future-demo').carousel({
+$('.carousel').carousel({
   interval: false
 });
 
@@ -27,6 +27,18 @@ jQuery(".block-title").fitText(1.5, { minFontSize: '24px', maxFontSize: '50px' }
 jQuery(".block-sub-title").fitText(2, { minFontSize: '24px', maxFontSize: '50px' });
 jQuery(".press-title").fitText(1.5, { minFontSize: '24px', maxFontSize: '50px' });
 jQuery(".item-title").fitText(2, { minFontSize: '14px', maxFontSize: '26px' });
+
+
+// three way pill
+$('.pills > #low').click(function(){
+  $('#three-way-demo-mode').removeClass().addClass('low');
+});
+$('.pills > #high').click(function(){
+  $('#three-way-demo-mode').removeClass().addClass('high');
+});
+$('.pills > #auto').click(function(){
+  $('#three-way-demo-mode').removeClass().addClass('auto');
+});
 
 
 // mist-animation
@@ -56,7 +68,7 @@ function flowSideAnime(){
   $('.flow-side-anime').css("background-position", "50% "+target_background_potition);
   pic_num1++;
   if (pic_num1 >= 15) pic_num1 = 0;
-  setTimeout("flowSideAnime()",100);
+  setTimeout("flowSideAnime()", 67);
 }
 function flowAngleAnime(){
   var target_height = (100/(20-1));
@@ -64,7 +76,7 @@ function flowAngleAnime(){
   $('.flow-angle-anime').css("background-position", "50% "+target_background_potition);
   pic_num2++;
   if (pic_num2 >= 20) pic_num2 = 0;
-  setTimeout("flowAngleAnime()",100);
+  setTimeout("flowAngleAnime()", 50);
 }
 
 
@@ -75,16 +87,21 @@ $('.scrollspy-nav li').on('activate.bs.scrollspy', function () {
 
 
 // capacity-count
-$('#stepper').zero();
 $('.capacity-scrollspy').on('activate.bs.scrollspy', function () {
   $(".capacity").addClass("active");
-  $('#stepper').play();
+  $('#stepper').zero().play();
 });
 
 
 // energy-chart-anime
 $('.energy-scroollspy').on('activate.bs.scrollspy', function () {
   $(".energy").addClass("active");
+});
+
+
+// easy-control
+$('.easy-control-scrollspy').on('activate.bs.scrollspy', function () {
+  $(".easy-control").addClass("active");
 });
 
 
